1cadeccf1e372b461f6c6cba85e430519f9a1055
suhanigupta25 <suhani259gupta@gmail.com>
2026-07-27 01:11:05 +0530

    Fix ts definitions not loading

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,10 +1,11 @@
-import { Request } from "express";
-import { TokenPayload } from "../services/auth.service";
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: TokenPayload;
-    }
+namespace Express {
+  interface Request {
+    user?: {
+      userId: string;
+      email: string;
+    };
   }
 }
+
+//YAYA does npm run dev not work? pnpm is faster and less bug prone, under the hood dono same
+//hogya ? commit the changes to git
